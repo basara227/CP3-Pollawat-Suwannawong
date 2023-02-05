a1576cf5b18b8d3bafe58fc7f19f034602ba5b3c
basara227 <124142629+basara227@users.noreply.github.com>
2023-02-05 18:21:13 +0700

    Create Exercise5_1_Pollwat_S.py

---
+++ Exercise5_1_Pollwat_S.py
@@ -0,0 +1,6 @@
+num1 = input("First number : ")
+num2 = input("Second number : ")
+print(num1,"+",num2,"=",int(num1)+int(num2))
+print(num1,"-",num2,"=",int(num1)-int(num2))
+print(num1,"*",num2,"=",int(num1)*int(num2))
+print(num1,"/",num2,"=",(int(num1)/int(num2)))
