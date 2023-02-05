bf934412d7b5cb417d1097df2f95e8581fa5326e
basara227 <124142629+basara227@users.noreply.github.com>
2023-02-05 15:29:12 +0700

    Create CP3.py

---
+++ CP3.py
@@ -0,0 +1,10 @@
+FE = 80.5
+GB = 85.4
+ICS = 90.1
+CP = 98.3
+print("--- Your Score ---")
+print("Foundation English :", FE)
+print("General Business :", GB)
+print("Introduction to Computer Systems :", ICS)
+print("Computer Programming :", CP)
+print("------------------")
